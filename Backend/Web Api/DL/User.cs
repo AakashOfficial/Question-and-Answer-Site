7e8e3abfc833d377a0f6a689da6516aa7b6c1446
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 17:17:58 +0530

    User Model

---
+++ Backend/Web Api/DL/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your First Name")]
+        public string UserFirstName { get; set; }
+
+        public string UserLastName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Email")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        public string UserPassword { get; set; }
+
+        public string UserProfilePicture { get; set; }
+        [Required]
+        public string UserActive { get; set; }
+
+        public DateTime CreationDate { get; set; }
+        public virtual IList<Question> Questions { get; set; }
+    }
+}
