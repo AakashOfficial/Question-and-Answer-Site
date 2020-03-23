8db3d827a99725c622ddc32e0a5c5cf5640ba3ad
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 17:24:51 +0530

    DB Context File

---
+++ Backend/Web Api/DL/QAContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DL
+{
+    class QAContext : DbContext
+    {
+        public DbSet<User> user { get; set; }
+
+        public DbSet<Question> question { get; set; }
+
+        public DbSet<Answer> answer { get; set; }
+
+        public DbSet<UserReaction> userreaction { get; set; }
+
+        public DbSet<Tags> tags { get; set; }
+    }
+}
