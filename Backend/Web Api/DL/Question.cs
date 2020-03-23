9a9748673a2336c764990e36ffa9e51fa3245346
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 17:19:06 +0530

    Question Model

---
+++ Backend/Web Api/DL/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    class Question
+    {
+        [Key]
+        public int QuestionId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Event Date")]
+        public string QuestionName { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int QuestionActive { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual IList<Answer> Answers { get; set; }
+    }
+}
