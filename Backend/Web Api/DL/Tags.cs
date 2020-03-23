ea112303630dddc585f2147a8c9c59e1b3422f91
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 17:23:51 +0530

    Tags Model

---
+++ Backend/Web Api/DL/Tags.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    class Tags
+    {
+        [Key]
+        public int QuestionTagId { get; set; }
+
+        public string TagName { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int TagActive { get; set; }
+
+        public virtual Question Question { get; set; }
+    }
+}
