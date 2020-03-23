f211ccfaad66b2c87188901cdd207277548602f4
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 15:42:58 +0530

    Frontend Worked

--- Frontend/my-app/src/app/app.module.ts
+++ Frontend/my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestcompComponent } from './testcomp/testcomp.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestcompComponent
+    TestcompComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
