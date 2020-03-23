d2f76369b03f7463e2aababbac6c7ec59b6d6e96
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 15:08:12 +0530

    Areas

---
+++ Backend/Web Api/Web Api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Web_Api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
