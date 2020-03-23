0f4cbf7a4d3505ad279ce92d915968f3b06947f0
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-23 17:23:16 +0530

    UserReaction Model

---
+++ Backend/Web Api/DL/UserReaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    class UserReaction
+    {
+        [Key]
+        public int ReactionId { get; set; }
+
+        public int ReactionType { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int ReactionActive { get; set; }
+
+        public int AnswerId { get; set; }
+
+        public virtual Answer Answer { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
