f99b526307f81a9368e5af8d971528d68c8c2b23
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 16:28:41 +0530

    User Business Layer

---
+++ Backend/Web Api/BL/UserOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DL;
+
+namespace BL
+{
+    public class UserOperations
+    {
+        private QAContext databaseContext;
+
+        public UserOperations()
+        {
+            databaseContext = new QAContext();
+        }
+
+        // For adding the User
+        public bool addUser(User user)
+        {
+            databaseContext.user.Add(user);
+            databaseContext.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<User> getUser(int id)
+        {
+            var result = databaseContext.user.ToList().Where(d => d.UserId == id);
+            return result;
+        }
+    }
+}
