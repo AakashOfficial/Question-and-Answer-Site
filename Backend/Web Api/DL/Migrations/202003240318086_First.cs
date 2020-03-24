925901501482587fbbed7d3d4caee7a62e408d9a
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 08:57:37 +0530

    Migrations

---
+++ Backend/Web Api/DL/Migrations/202003240318086_First.cs
@@ -0,0 +1,110 @@
+﻿namespace DL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class First : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Answers",
+                c => new
+                    {
+                        AnswerId = c.Int(nullable: false, identity: true),
+                        AnswerName = c.String(nullable: false),
+                        CreationDate = c.DateTime(nullable: false),
+                        AnswerActive = c.Int(nullable: false),
+                        QuestionId = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.AnswerId)
+                .ForeignKey("dbo.Questions", t => t.QuestionId, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.QuestionId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Questions",
+                c => new
+                    {
+                        QuestionId = c.Int(nullable: false, identity: true),
+                        QuestionName = c.String(nullable: false),
+                        CreationDate = c.DateTime(nullable: false),
+                        QuestionActive = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.QuestionId)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        UserFirstName = c.String(nullable: false),
+                        UserLastName = c.String(),
+                        UserEmail = c.String(nullable: false),
+                        UserPassword = c.String(nullable: false),
+                        UserProfilePicture = c.String(),
+                        UserActive = c.Int(nullable: false),
+                        CreationDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+            CreateTable(
+                "dbo.UserReactions",
+                c => new
+                    {
+                        ReactionId = c.Int(nullable: false, identity: true),
+                        ReactionType = c.Int(nullable: false),
+                        CreationDate = c.DateTime(nullable: false),
+                        ReactionActive = c.Int(nullable: false),
+                        AnswerId = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ReactionId)
+                .ForeignKey("dbo.Answers", t => t.AnswerId, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.AnswerId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        QuestionTagId = c.Int(nullable: false, identity: true),
+                        TagName = c.String(),
+                        QuestionId = c.Int(nullable: false),
+                        CreationDate = c.DateTime(nullable: false),
+                        TagActive = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.QuestionTagId)
+                .ForeignKey("dbo.Questions", t => t.QuestionId, cascadeDelete: true)
+                .Index(t => t.QuestionId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tags", "QuestionId", "dbo.Questions");
+            DropForeignKey("dbo.Answers", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserReactions", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserReactions", "AnswerId", "dbo.Answers");
+            DropForeignKey("dbo.Questions", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Answers", "QuestionId", "dbo.Questions");
+            DropIndex("dbo.Tags", new[] { "QuestionId" });
+            DropIndex("dbo.UserReactions", new[] { "UserId" });
+            DropIndex("dbo.UserReactions", new[] { "AnswerId" });
+            DropIndex("dbo.Questions", new[] { "UserId" });
+            DropIndex("dbo.Answers", new[] { "UserId" });
+            DropIndex("dbo.Answers", new[] { "QuestionId" });
+            DropTable("dbo.Tags");
+            DropTable("dbo.UserReactions");
+            DropTable("dbo.Users");
+            DropTable("dbo.Questions");
+            DropTable("dbo.Answers");
+        }
+    }
+}
---
+++ Backend/Web Api/DL/Migrations/Configuration.cs
@@ -0,0 +1,72 @@
+﻿namespace DL.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DL.QAContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DL.QAContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+
+            var users = new List<User>
+            {
+                new User{ UserFirstName = "Aakash" , UserLastName = "Tyagi", UserEmail = "aakash.tyagi@nagarro.com" , UserPassword = "Passw0rd@123" , UserActive = 1 , CreationDate = DateTime.Parse("2020-03-24") , UserId = 1 ,},
+                new User { UserFirstName = "Anshika", UserLastName = "Tyagi", UserEmail = "anshikatyagi2007@outlook.com", UserPassword = "Passw0rd@123", UserActive = 1, CreationDate = DateTime.Parse("2020-03-24"), UserId = 2 , }
+            };
+
+            users.ForEach(s => context.user.AddOrUpdate(p => p.UserEmail, s));
+            context.SaveChanges();
+
+
+            var question = new List<Question> 
+            {
+                new Question{ QuestionId = 1 , UserId = 1 , QuestionName = "What is the capital of India" , CreationDate = DateTime.Parse("2020-03-24") , QuestionActive = 1 , },
+                new Question{ QuestionId = 2 , UserId = 2 , QuestionName = "What is the capital of Uttar Pradesh" , CreationDate = DateTime.Parse("2020-03-24") , QuestionActive = 1 , }
+            };
+
+            question.ForEach(s => context.question.AddOrUpdate(p => p.QuestionName, s));
+            context.SaveChanges();
+
+            var answer = new List<Answer>
+            {
+                new Answer{ AnswerId = 1 , QuestionId = 1 , UserId = 2 , AnswerName = "Delhi" , CreationDate = DateTime.Parse("2020-03-24") , AnswerActive = 1,},
+                new Answer{AnswerId = 2 , QuestionId = 2 , UserId = 1 , AnswerName = "Lucknow" , CreationDate = DateTime.Parse("2020-03-24") , AnswerActive = 1,}
+            };
+
+            answer.ForEach(s => context.answer.AddOrUpdate(p => p.AnswerName, s));
+            context.SaveChanges();
+
+            var tags = new List<Tags>
+            {
+                new Tags{ QuestionTagId = 1 , TagName = "Capital" , CreationDate = DateTime.Parse("2020-03-24") , QuestionId = 1 , TagActive = 1 , },
+                new Tags{ QuestionTagId = 2 , TagName = "Capital" , CreationDate = DateTime.Parse("2020-03-24") , QuestionId = 2 , TagActive = 1 , }
+            };
+
+            tags.ForEach(s => context.tags.AddOrUpdate(p => p.TagName, s));
+            context.SaveChanges();
+
+            var userreaction = new List<UserReaction>
+            {
+                new UserReaction{ ReactionId = 1 , UserId = 2 , ReactionType = 1 , CreationDate = DateTime.Parse("2020-03-24") , AnswerId = 1 , ReactionActive = 1 , },
+                new UserReaction{ ReactionId = 2 , UserId = 1 , ReactionType = 0 , CreationDate = DateTime.Parse("2020-03-24") , AnswerId = 1 , ReactionActive = 1 , },
+                new UserReaction{ ReactionId = 3 , UserId = 2 , ReactionType = 0 , CreationDate = DateTime.Parse("2020-03-24") , AnswerId = 2 , ReactionActive = 1 , },
+                new UserReaction{ ReactionId = 4 , UserId = 1 , ReactionType = 1 , CreationDate = DateTime.Parse("2020-03-24") , AnswerId = 2 , ReactionActive = 1 , }
+            };
+
+            userreaction.ForEach(s => context.userreaction.AddOrUpdate(p => p.ReactionType, s));
+        }
+    }
+}
