ed820a9f923987758f471c52d954c5e8bd440519
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 16:29:48 +0530

    All Question Web API

---
+++ Backend/Web Api/Web Api/Controllers/QuestionController.cs
@@ -0,0 +1,42 @@
+﻿using BL;
+using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_Api.Controllers
+{
+    public class QuestionController : ApiController
+    {
+        private QuestionOperations questionoperation;
+        public QuestionController()
+        {
+            questionoperation = new QuestionOperations();
+        }
+
+        // GET api/<controller>/5
+        public List<Question> Get(int id)
+        {
+            var result = questionoperation.getQuestions();
+            return result;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
