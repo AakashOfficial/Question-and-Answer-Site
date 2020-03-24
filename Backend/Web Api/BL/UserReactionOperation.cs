93971ff8df4a20578b8138c9943d17598fe1c163
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 22:12:20 +0530

    User Reaction Business Layer

---
+++ Backend/Web Api/BL/UserReactionOperation.cs
@@ -0,0 +1,18 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    class UserReactionOperation
+    {
+        private QAContext databaseContext;
+
+        public UserReactionOperation() {
+            databaseContext = new QAContext();
+        }
+    }
+}
