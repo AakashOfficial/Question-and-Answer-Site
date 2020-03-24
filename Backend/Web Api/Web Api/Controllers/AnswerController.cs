bb5a036e785fe74070945a4493aeb7e4ffeeab04
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 22:08:22 +0530

    Answer Controller

---
+++ Backend/Web Api/Web Api/Controllers/AnswerController.cs
@@ -0,0 +1,26 @@
+﻿using BL;
+using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Web_Api.Controllers
+{
+    public class AnswerController : ApiController
+    {
+        private AnswerOperations answerOperations;
+        public AnswerController()
+        {
+            answerOperations = new AnswerOperations();
+        }
+        // GET: Answer
+        public List<Answer> get(int id)
+        {
+            var result = answerOperations.getAnswers(id);
+            return result;            
+        }
+    }
+}
