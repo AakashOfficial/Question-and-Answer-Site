952d6754171e74967d92f0eb225222f806f3c84a
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 22:18:48 +0530

    User Reaction Business Layer

--- Backend/Web Api/BL/UserReactionOperation.cs
+++ Backend/Web Api/BL/UserReactionOperation.cs
@@ -1,6 +1,7 @@
 ﻿using DL;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,28 @@ namespace BL
 
         public UserReactionOperation() {
             databaseContext = new QAContext();
+        }
+
+        public List<UserReaction> getReaction(int id)
+        {
+            var output = databaseContext.userreaction.Where(d => d.AnswerId == id).ToList();
+            return output;
+        }
+
+        public bool addReaction(UserReaction userreaction)
+        {
+            databaseContext.userreaction.Add(userreaction);
+            return true;
+        }
+
+        public bool editReaction(UserReaction userreaction)
+        {
+            databaseContext.Entry(userreaction).State = EntityState.Modified;
+            return true;
+        }
+
+        public bool deleteReaction (int id){
+            return true;
         }
     }
 }
