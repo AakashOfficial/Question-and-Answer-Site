04e343a207e8670c0321c5d2752ae6fd763e893b
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-24 16:28:20 +0530

    Question Business Layer

---
+++ Backend/Web Api/BL/QuestionOperations.cs
@@ -0,0 +1,27 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class QuestionOperations
+    {
+        private QAContext databaseContext;
+
+        public QuestionOperations()
+        {
+            databaseContext = new QAContext();
+        }
+
+        public List<Question> getQuestions()
+        {
+            var result = databaseContext.question.ToList();
+            return result;
+        }
+
+
+    }
+}
