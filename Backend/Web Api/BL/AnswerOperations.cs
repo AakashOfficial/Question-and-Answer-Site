ed76d7ce903802eaa538b5621089258618c2f7aa
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-27 15:28:39 +0530

    Answer Business Layer

--- Backend/Web Api/BL/AnswerOperations.cs
+++ Backend/Web Api/BL/AnswerOperations.cs
@@ -19,42 +19,9 @@ namespace BL
 
         public List<Answer> getAnswers(int id)
         {
-            var result = databaseContext.answer.Where(d => d.QuestionId == id).ToList();
+            var result = databaseContext.answer.Where(d => d.QuestionId == id && d.AnswerActive == 1).ToList();
             return result;
         }
 
-        // for getting the count of the Answers of a Question
-        public int getAnswerCount(int id) {
-            int count = 0;
-
-            count = databaseContext.answer.Where(d => d.QuestionId == id).Count();
-            if(count > 0){
-                return count;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        // for edit the answer
-        public bool editAnswer(Answer answer)
-        {
-            databaseContext.Entry(answer).State = EntityState.Modified;
-            databaseContext.SaveChanges();
-            return true;
-        }
-
-        // for add the answer
-        public bool addAnswer(Answer answer)
-        {
-            databaseContext.answer.Add(answer);
-            return true;
-        }
-
-        public bool deleteAnswer(int id)
-        {
-            return true;
-        }
     }
 }
