57e56f7424a2729a3b6fbc8ca1ce798f89a334ea
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-27 15:36:33 +0530

    tags model

---
+++ Frontend/my-app/src/app/Model/user.ts
@@ -0,0 +1,12 @@
+
+export class User
+{
+    UserId   :number;
+    UserFirstName :string;
+    UserLastName :string;
+    UserEmail :string;
+    UserPassword :string;
+    UserProfilePicture :string;
+    UserActive : number;
+    CreationDate : string;
+}
