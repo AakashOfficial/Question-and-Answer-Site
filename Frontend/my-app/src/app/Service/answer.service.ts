8f4a7776a2e4dab9a616364f3c48cde26500576c
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-27 21:11:22 +0530

    answer service

--- Frontend/my-app/src/app/Service/answer.service.ts
+++ Frontend/my-app/src/app/Service/answer.service.ts
@@ -5,6 +5,7 @@ import { catchError, tap, map } from 'rxjs/operators';
 import {User} from 'src/app/Model/user';
 import {Answer} from 'src/app/Model/Answer';
 import {UserReaction} from 'src/app/Model/userreaction';
+import {Question} from 'src/app/Model/question';
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +13,16 @@ import {UserReaction} from 'src/app/Model/userreaction';
 
 export class AnswerService {
 
-    private productsUrl ='http://localhost:63121/api/Answer/';
+    private baseUrl ='https://localhost:44302/api/answer/';
+    private completeUrl = '' ;
 
     constructor(private http: HttpClient) { }
+
+    // get all answer by the questionId
+    getAnswers(QuestionID) {
+        this.completeUrl = this.baseUrl + 'getAllAnswer?id=' ;
+        return this.http.get(this.completeUrl+QuestionID)
+    }
+
+
 }
