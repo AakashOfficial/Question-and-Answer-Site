5c95190f713be370b2bc4c9ca4318df43a998f29
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-31 23:00:24 +0530

    Answer Services

--- Frontend/my-app/src/app/Service/answer.service.ts
+++ Frontend/my-app/src/app/Service/answer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient,HttpHeaders} from '@angular/common/http';
+import {HttpClientModule,HttpClient,HttpHeaders} from '@angular/common/http';
 import { Observable,of ,throwError, from} from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import {User} from 'src/app/Model/user';
