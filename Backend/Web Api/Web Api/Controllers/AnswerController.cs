c029b17ec9844ebf8037b3af5487e20d82d34781
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-31 22:57:26 +0530

    Answer Controller

--- Backend/Web Api/Web Api/Controllers/AnswerController.cs
+++ Backend/Web Api/Web Api/Controllers/AnswerController.cs
@@ -5,9 +5,11 @@ using System.Web.Http;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Http.Cors;
 
 namespace Web_Api.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class AnswerController : ApiController
     {
         private AnswerOperations answerOperations;
