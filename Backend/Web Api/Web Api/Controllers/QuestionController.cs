f50475b145e171a775534cfb1dd923ca8e7460ff
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-31 22:59:05 +0530

    Question Controller

--- Backend/Web Api/Web Api/Controllers/QuestionController.cs
+++ Backend/Web Api/Web Api/Controllers/QuestionController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Web_Api.Controllers
 {
+    [EnableCors(origins: "*", headers:"*",methods:"*")]
     public class QuestionController : ApiController
     {
         private QuestionOperations questionoperation;
