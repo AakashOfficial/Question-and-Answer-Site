233b8496e8e9a9314ba0725b6cfba420732117f8
AakashOfficial <aakashtyagi1996@outlook.com>
2020-03-31 22:07:29 +0530

    Tags Operations

---
+++ Backend/Web Api/BL/TagsOperations.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DL;
+
+namespace BL
+{
+    class TagsOperations
+    {
+        private QAContext databaseContext;
+
+        public TagsOperations()
+        {
+            databaseContext = new QAContext();
+        }
+
+        public int addTags(Tags tags)
+        {
+            var output = databaseContext.tags.Any(x => x.TagName == tags.TagName);
+
+            if (output)
+            {
+                return databaseContext.tags.FirstOrDefault(x => x.TagName == tags.TagName).TagId;
+            }
+
+            databaseContext.tags.Add(tags);
+            databaseContext.SaveChanges();
+
+            return databaseContext.tags.FirstOrDefault(x => x.TagName == tags.TagName).TagId;
+        }
+
+    }
+}
