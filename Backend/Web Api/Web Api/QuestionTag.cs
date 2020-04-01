5e96066482008b9650dfc06e3ccb9dd95897af48
AakashOfficial <aakashtyagi1996@outlook.com>
2020-04-01 21:07:43 +0530

    Backend Complete

---
+++ Backend/Web Api/Web Api/QuestionTag.cs
@@ -0,0 +1,17 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Api
+{
+
+    public class QuestionTag
+    {
+        // question table object 
+        public Question question { get; set; }
+        // tag table object
+        public Tags tags { get; set; }
+    }
+}
