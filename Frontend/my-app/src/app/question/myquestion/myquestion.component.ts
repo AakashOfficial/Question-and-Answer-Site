3be3755e51fb25b7b1d17505cc2465e75c83f94d
AakashOfficial <aakashtyagi1996@outlook.com>
2020-04-01 21:32:10 +0530

    myquestion component

---
+++ Frontend/my-app/src/app/question/myquestion/myquestion.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myquestion',
+  templateUrl: './myquestion.component.html',
+  styleUrls: ['./myquestion.component.css']
+})
+export class MyquestionComponent implements OnInit {
+
+    ngOnInit() {
+        
+    }
+}
